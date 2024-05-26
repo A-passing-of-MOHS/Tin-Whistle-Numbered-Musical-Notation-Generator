7b4cc58586a9ec82402e875f13d39c9af7d249e8
zonghao <1390014330@qq.com>
2024-05-26 16:53:54 +0800

    打包设置

--- forge.config.js
+++ forge.config.js
@@ -5,15 +5,6 @@ module.exports = {
   packagerConfig: {
     asar: true,
   },
-  // packagerConfig: {
-  //   name: 'APP_NAME',
-  //   // 不加扩展名，MAC 会自动查找 .icns、WIN 使用 .ico
-  //   icon: './icon/icon',
-  //   // 最终包不使用的代码，不要打入 asar
-  //   ignore: [/\.yarn/, /src\/render/],
-  //   appBundleId: `com.xxx.xxx`,
-  //   appCopyright: `Copyright © 2023 ${packageJson.author}`
-  // },
   rebuildConfig: {},
   makers: [
     // windows 上的打包
