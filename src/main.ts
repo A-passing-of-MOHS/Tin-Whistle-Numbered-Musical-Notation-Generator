7ce514625e6b5cf173bc290c88314a9e87350bf8
zonghao <1390014330@qq.com>
2024-05-26 17:05:24 +0800

    提示样式

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import './style.css'
 import router from './router'
 import App from './App.vue'
 import "element-plus/theme-chalk/el-message-box.css";
+import { ElMessage, ElMessageBox } from 'element-plus';
+import 'element-plus/theme-chalk/el-message.css';
+import 'element-plus/theme-chalk/el-message-box.css';
+
 const app =createApp(App)
 app.use(router)
 app.mount('#app')
-
+app.use(ElMessage);
+app.use(ElMessageBox);
